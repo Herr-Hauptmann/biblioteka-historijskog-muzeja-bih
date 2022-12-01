8487b88c7b38c27a0ae26440a9fdf2e586f63286
Herr-Hauptmann <tarik.horoz@gmail.com>
2022-12-01 13:15:29 +0100

    add bootstrap

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,5 +1,6 @@
-import './bootstrap';
 import '../css/app.css';
+// import "bootstrap/dist/css/bootstrap.min.css"
+// import "bootstrap"
 
 import { createApp, h } from 'vue';
 import { createInertiaApp } from '@inertiajs/inertia-vue3';
--- routes/web.php
+++ routes/web.php
@@ -1,29 +1,13 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\HomeController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+Route::get('/', [HomeController::class, 'index'])->name('home');
 
-Route::get('/', function () {
-    return Inertia::render('Welcome', [
-        'canLogin' => Route::has('login'),
-        'canRegister' => Route::has('register'),
-        'laravelVersion' => Application::VERSION,
-        'phpVersion' => PHP_VERSION,
-    ]);
-});
 
 Route::get('/dashboard', function () {
     return Inertia::render('Dashboard');
