2e86cdf1edd75595bf54df1ed0444a48e148a0cb
Herr-Hauptmann <tarik.horoz@gmail.com>
2022-12-01 01:07:59 +0100

    add laravel breeze

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,13 +1,20 @@
 <!DOCTYPE html>
-<html>
-  <head>
-    <meta charset="utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
-    @vite('resources/css/app.css')
-    @inertiaHead
-  </head>
-  <body>
-    @inertia
-    @vite('resources/js/app.js')
-  </body>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title inertia>{{ config('app.name', 'Laravel') }}</title>
+
+        <!-- Fonts -->
+        <link rel="stylesheet" href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap">
+
+        <!-- Scripts -->
+        @routes
+        @vite(['resources/js/app.js', "resources/js/Pages/{$page['component']}.vue"])
+        @inertiaHead
+    </head>
+    <body class="font-sans antialiased">
+        @inertia
+    </body>
 </html>
