9b5af1b6963c8469c7a91478f68dc1371dc2f2cd
Herr-Hauptmann <tarik.horoz@gmail.com>
2022-12-20 01:58:03 +0100

    refactor search bar as component

--- resources/js/Layouts/AuthenticatedLayout.vue
+++ resources/js/Layouts/AuthenticatedLayout.vue
@@ -1,6 +1,5 @@
 <script setup>
 import { ref } from 'vue';
-import ApplicationLogo from '@/Components/ApplicationLogo.vue';
 import Dropdown from '@/Components/Dropdown.vue';
 import DropdownLink from '@/Components/DropdownLink.vue';
 import NavLink from '@/Components/NavLink.vue';
--- resources/js/Pages/Books/BooksIndex.vue
+++ resources/js/Pages/Books/BooksIndex.vue
@@ -1,42 +1,30 @@
 <script setup>
 import { Head, Link } from "@inertiajs/inertia-vue3";
-import {Inertia} from "@inertiajs/inertia"
-import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout.vue";
 import Pagination from "@/Shared/Pagination.vue";
-import { ref, watch, computed } from "vue";
-import debounce from 'lodash/debounce';
+import { computed } from "vue";
+import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout.vue"
+import SearchBar from "@/Components/SearchBar.vue";
 
 let props = defineProps({
     filters: Object,
     books: Object
 });
 
-let placeholder = "Pretraži knjige";
-let search = ref(props.filters.search);
+const placeholder = "Pretraži knjige";
 
 let paginationData = computed(()=>{
     let pag = JSON.parse(JSON.stringify(props.books));
     delete pag.data;
     return pag;
 });
-
-watch(search, debounce (function (value){
-    Inertia.get(route('books.index'), {
-        search: value
-    }, {
-        preserveState: true,
-        replace: true
-    });
-}, 250));
-
-
-
 </script>
 
 <template>
     <Head title="Knjige" />
 
     <AuthenticatedLayout>
+
+        <!-- Layout header -->
         <template #header>
             <div class="flex justify-between">
                 <h2 class="font-semibold text-xl text-gray-800 leading-tight my-auto">
@@ -51,25 +39,13 @@ watch(search, debounce (function (value){
             </div>
         </template>
 
+        <!-- Main content -->
         <div class="pb-12 pt-4">
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
                 <div class="bg-white overflow-x-auto relative shadow-sm sm:rounded-lg">
-                    <div class="p-4 bg-white dark:bg-gray-900">
-                        <label for="table-search" class="sr-only">Pretraga</label>
-                        <div class="relative mt-1">
-                            <div class="flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none">
-                                <svg class="w-5 h-5 text-gray-500 dark:text-gray-400" aria-hidden="true"
-                                    fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                                    <path fill-rule="evenodd"
-                                        d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
-                                        clip-rule="evenodd"></path>
-                                </svg>
-                            </div>
-                            <input type="text" id="table-search"
-                                class="block p-2 pl-10 w-80 text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                                v-model="search" :placeholder="placeholder"/>
-                        </div>
-                    </div>
+                    <!-- Search bar -->
+                    <SearchBar :placeholder="placeholder" :filters="filters" :route="route('books.index')"/>
+                    <!-- Table -->
                     <table class="table-fixed md:table-auto w-full text-sm text-left text-gray-500 dark:text-gray-400">
                         <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
                             <tr>
