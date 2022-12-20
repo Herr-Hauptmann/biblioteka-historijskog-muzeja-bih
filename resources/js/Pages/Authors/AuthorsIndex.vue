402bd51f048b43650bb24bad89f1a4b7519a8315
Herr-Hauptmann <tarik.horoz@gmail.com>
2022-12-21 00:07:42 +0100

    delete link for creating authors from authors.index

--- resources/js/Pages/Authors/AuthorsIndex.vue
+++ resources/js/Pages/Authors/AuthorsIndex.vue
@@ -30,12 +30,6 @@ let paginationData = computed(()=>{
                 <h2 class="font-semibold text-xl text-gray-800 leading-tight my-auto">
                     Autori
                 </h2>
-                <Link :href="route('authors.create')">
-                <button
-                    class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-                    Dodaj autora
-                </button>
-                </Link>
             </div>
         </template>
 
