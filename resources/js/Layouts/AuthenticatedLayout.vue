a93fcfbef6f22a607ab36f01fb0e55a10502bda3
Herr-Hauptmann <tarik.horoz@gmail.com>
2023-02-22 15:13:34 +0100

    implement prettier scrollbar

--- resources/js/Layouts/AuthenticatedLayout.vue
+++ resources/js/Layouts/AuthenticatedLayout.vue
@@ -39,3 +39,25 @@ watch(() => usePage().props.value.flash.message , (newValue, oldValue) => {
         </div>
     </div>
 </template>
+
+<style>
+::-webkit-scrollbar-track
+{
+	-webkit-box-shadow: inset 0 0 6px rgba(0,0,0,0.3);
+	border-radius: 10px;
+	background-color: #F5F5F5;
+}
+
+::-webkit-scrollbar
+{
+	width: 8px;
+	background-color: #F5F5F5;
+}
+
+::-webkit-scrollbar-thumb
+{
+	border-radius: 10px;
+	-webkit-box-shadow: inset 0 0 6px rgba(0,0,0,.3);
+	background-color: #555;
+}
+</style>
