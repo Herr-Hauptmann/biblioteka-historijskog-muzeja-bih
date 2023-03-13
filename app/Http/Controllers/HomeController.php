680d4af9010317d99b314ff4e2cb392c58aef2cb
Herr-Hauptmann <tarik.horoz@gmail.com>
2023-03-13 19:54:56 +0100

    compress photos

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -33,19 +33,19 @@ class HomeController extends Controller
 
     public function about(){
         return Inertia::render('About',[
-            'photo1_url' => Vite::asset('resources/images/about/slika1.JPG'),
-            'photo2_url' => Vite::asset('resources/images/about/slika2.JPG'),
-            'photo3_url' => Vite::asset('resources/images/about/slika3.JPG'),
-            'photo4_url' => Vite::asset('resources/images/about/slika4.JPG'),
-            'photo5_url' => Vite::asset('resources/images/about/slika5.JPG'),
-            'photo6_url' => Vite::asset('resources/images/about/slika6.jpg'),
-            'photo7_url' => Vite::asset('resources/images/about/slika7.jpg'),
+            'photo1_url' => Vite::asset('resources/images/about/slika1-min.JPG'),
+            'photo2_url' => Vite::asset('resources/images/about/slika2-min.JPG'),
+            'photo3_url' => Vite::asset('resources/images/about/slika3-min.JPG'),
+            'photo4_url' => Vite::asset('resources/images/about/slika4-min.JPG'),
+            'photo5_url' => Vite::asset('resources/images/about/slika5-min.JPG'),
+            'photo6_url' => Vite::asset('resources/images/about/slika6-min.jpg'),
+            'photo7_url' => Vite::asset('resources/images/about/slika7-min.jpg'),
         ]);
     }
 
     public function faq(){
         return Inertia::render('Faq',[
-            'photo_url' => Vite::asset('resources/images/about/slika1.JPG'),
+            'photo_url' => Vite::asset('resources/images/about/slika1-min.JPG'),
         ]);
     }
 }
