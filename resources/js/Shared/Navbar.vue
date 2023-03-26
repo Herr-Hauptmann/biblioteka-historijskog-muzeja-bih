d4e1d9c0f3d8e2c831811c7dd2ac4243478bad81
Herr-Hauptmann <tarik.horoz@gmail.com>
2023-03-27 01:28:19 +0200

    make navbar responsive

--- resources/js/Shared/Navbar.vue
+++ resources/js/Shared/Navbar.vue
@@ -91,9 +91,9 @@
         <div class="divide-y-2 divide-gray-50 rounded-lg bg-white shadow-lg ring-1 ring-black ring-opacity-5">
           <div class="px-5 pt-5 pb-6">
             <div class="flex items-center justify-between">
-              <div>
-                <img class="h-8 w-auto" :src="$page.props.logo_url" alt="Biblioteka Historijskog muzeja BiH" />
-              </div>
+              <Link :href="route('home')">
+                <img class="h-8 w-auto" :src="$page.props.images.logoSmall" alt="Biblioteka Historijskog muzeja BiH" />
+              </Link>
               <div class="-mr-2">
                 <PopoverButton class="inline-flex items-center justify-center rounded-md bg-white p-2 text-gray-400 hover:bg-gray-100 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
                   <span class="sr-only">Zatvori meni</span>
@@ -110,20 +110,13 @@
               </nav>
             </div>
           </div>
-          <div class="space-y-6 py-6 px-5">
-            <div class="grid grid-cols-2 gap-y-4 gap-x-8">
-              <a href="#" class="text-base font-medium text-gray-900 hover:text-gray-700">Pricing</a>
-
-              <a href="#" class="text-base font-medium text-gray-900 hover:text-gray-700">Docs</a>
-              <Link v-for="item in resources" :key="item.name" :href="item.href"  class="text-base font-medium text-gray-900 hover:text-gray-700">{{ item.name }}</Link>
+          <div class="space-y-6 pb-6 px-5">
+            <div class="">
+              <h3 class="text-base font-medium text-gray-500 pb-3">Priče iz biblioteke</h3>
+              <Link v-for="item in recentPosts" :key="item.title" :href="route('news.show', item.id)"  class="block pb-3 text-base font-medium text-gray-900 hover:text-gray-700">{{ item.title }}</Link>
             </div>
             <div>
-              <a href="#" class="flex w-full items-center justify-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-indigo-700">Sign up</a>
-              <p class="mt-6 text-center text-base font-medium text-gray-500">
-                Existing customer?
-                {{ ' ' }}
-                <a href="#" class="text-indigo-600 hover:text-indigo-500">Sign in</a>
-              </p>
+              <Link :href="route('login')" class="flex w-full items-center justify-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-indigo-700">Admin</Link>
             </div>
           </div>
         </div>
@@ -138,16 +131,12 @@ import {Link} from '@inertiajs/inertia-vue3';
 import {ref} from 'vue';
 import { Popover, PopoverButton, PopoverGroup, PopoverPanel } from '@headlessui/vue';
 import {
-  ArrowPathIcon,
+  MagnifyingGlassIcon,
   Bars3Icon,
   XMarkIcon,
-  BookOpenIcon,
-  PencilIcon,
-  ChartBarIcon,
-  CursorArrowRaysIcon,
+  InformationCircleIcon,
   QuestionMarkCircleIcon,
   LightBulbIcon,
-  Squares2X2Icon,
   UserIcon,
   PencilSquareIcon
 } from '@heroicons/vue/24/outline'
@@ -156,34 +145,48 @@ import {
   ArrowLeftOnRectangleIcon,
  } from '@heroicons/vue/24/solid'
 
+
+ //Hamburger menu content
 const solutions = [
   {
-    name: 'Analytics',
-    description: 'Get a better understanding of where your traffic is coming from.',
-    href: '#',
-    icon: ChartBarIcon,
+    name: 'Naslovi', 
+    description: 'Pregledaj naslove', 
+    href: route('books.list'), 
+    icon: MagnifyingGlassIcon
   },
   {
-    name: 'Engagement',
-    description: 'Speak directly to your customers in a more meaningful way.',
-    href: '#',
-    icon: CursorArrowRaysIcon,
+    name: 'O biblioteci', 
+    description: 'Detaljne informacije o našoj biblioteci', 
+    href: route('about'), 
+    icon: InformationCircleIcon
   },
-  { name: 'Security', description: "Your customers' data will be safe and secure.", href: '#', icon: LightBulbIcon },
+  // {
+  //   name: 'Pridruži se',
+  //   description: 'Postanite član naše biblioteke',
+  //   href: '#',
+  //   icon: PencilIcon,
+  // },
+  { 
+    name: 'Pošalji upit', 
+    description: 'Postavite nam pitanje', 
+    href: "#", 
+    icon: QuestionMarkCircleIcon
+  },
   {
-    name: 'Integrations',
-    description: "Connect with third-party tools that you're already using.",
-    href: '#',
-    icon: Squares2X2Icon,
+    name: 'FAQ',
+    description: 'Odgovori na često postavljena pitanja',
+    href: route('faq'),
+    icon: LightBulbIcon,
   },
-  {
-    name: 'Automations',
-    description: 'Build strategic funnels that will drive your customers to convert',
-    href: '#',
-    icon: ArrowPathIcon,
-  },
+  // {
+  //   name: 'Doniraj knjigu',
+  //   description: 'Doprinesite našoj biblioteci',
+  //   href: '#',
+  //   icon: BookOpenIcon,
+  // },
 ]
 
+//Full size navbar content
 const resources = [
   // {
   //   name: 'Pridruži se',
@@ -191,12 +194,12 @@ const resources = [
   //   href: '#',
   //   icon: PencilIcon,
   // },
-  // { 
-  //   name: 'Pošalji upit', 
-  //   description: 'Postavite nam pitanje', 
-  //   href: "#", 
-  //   icon: QuestionMarkCircleIcon
-  // },
+  { 
+    name: 'Pošalji upit', 
+    description: 'Postavite nam pitanje', 
+    href: "#", 
+    icon: QuestionMarkCircleIcon
+  },
   {
     name: 'FAQ',
     description: 'Odgovori na često postavljena pitanja',
