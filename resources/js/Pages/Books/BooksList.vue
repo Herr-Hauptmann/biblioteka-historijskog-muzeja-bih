21db20a842173bcce7f7acf9ff269545ac7a9556
Herr-Hauptmann <tarik.horoz@gmail.com>
2023-03-27 01:45:48 +0200

    make books list mobile friendly

--- resources/js/Pages/Books/BooksList.vue
+++ resources/js/Pages/Books/BooksList.vue
@@ -39,8 +39,8 @@ let paginationData = computed(() => {
                 </div>
                 
                 <!-- Table -->
-                <table class="table-fixed md:table-auto w-full text-sm text-left text-gray-500 dark:text-gray-400">
-                    <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                <table class="table-fixed md:table-auto text-sm text-left text-gray-500">
+                    <thead class="text-xs text-gray-700 uppercase bg-gray-50">
                         <tr>
                             <th scope="col" class="py-3 px-6 max-w-18">
                                 Naslov
@@ -48,41 +48,21 @@ let paginationData = computed(() => {
                             <th scope="col" class="py-3 px-6 hidden md:table-cell">
                                 <div class="flex items-center">
                                     Autor
-                                    <a href="#"><svg xmlns="http://www.w3.org/2000/svg" class="ml-1 w-3 h-3"
-                                            aria-hidden="true" fill="currentColor" viewBox="0 0 320 512">
-                                            <path
-                                                d="M27.66 224h264.7c24.6 0 36.89-29.78 19.54-47.12l-132.3-136.8c-5.406-5.406-12.47-8.107-19.53-8.107c-7.055 0-14.09 2.701-19.45 8.107L8.119 176.9C-9.229 194.2 3.055 224 27.66 224zM292.3 288H27.66c-24.6 0-36.89 29.77-19.54 47.12l132.5 136.8C145.9 477.3 152.1 480 160 480c7.053 0 14.12-2.703 19.53-8.109l132.3-136.8C329.2 317.8 316.9 288 292.3 288z" />
-                                        </svg></a>
                                 </div>
                             </th>
                             <th scope="col" class="py-3 px-6 hidden md:table-cell">
                                 <div class="flex items-center">
                                     Izdavač
-                                    <a href="#"><svg xmlns="http://www.w3.org/2000/svg" class="ml-1 w-3 h-3"
-                                            aria-hidden="true" fill="currentColor" viewBox="0 0 320 512">
-                                            <path
-                                                d="M27.66 224h264.7c24.6 0 36.89-29.78 19.54-47.12l-132.3-136.8c-5.406-5.406-12.47-8.107-19.53-8.107c-7.055 0-14.09 2.701-19.45 8.107L8.119 176.9C-9.229 194.2 3.055 224 27.66 224zM292.3 288H27.66c-24.6 0-36.89 29.77-19.54 47.12l132.5 136.8C145.9 477.3 152.1 480 160 480c7.053 0 14.12-2.703 19.53-8.109l132.3-136.8C329.2 317.8 316.9 288 292.3 288z" />
-                                        </svg></a>
                                 </div>
                             </th>
                             <th scope="col" class="py-3 px-6 hidden md:table-cell">
                                 <div class="flex items-center">
                                     Mjesto izdavanja
-                                    <a href="#"><svg xmlns="http://www.w3.org/2000/svg" class="ml-1 w-3 h-3"
-                                            aria-hidden="true" fill="currentColor" viewBox="0 0 320 512">
-                                            <path
-                                                d="M27.66 224h264.7c24.6 0 36.89-29.78 19.54-47.12l-132.3-136.8c-5.406-5.406-12.47-8.107-19.53-8.107c-7.055 0-14.09 2.701-19.45 8.107L8.119 176.9C-9.229 194.2 3.055 224 27.66 224zM292.3 288H27.66c-24.6 0-36.89 29.77-19.54 47.12l132.5 136.8C145.9 477.3 152.1 480 160 480c7.053 0 14.12-2.703 19.53-8.109l132.3-136.8C329.2 317.8 316.9 288 292.3 288z" />
-                                        </svg></a>
                                 </div>
                             </th>
                             <th>
                                 <div class="flex items-center">
                                     Godina izdavanja
-                                    <a href="#"><svg xmlns="http://www.w3.org/2000/svg" class="ml-1 w-3 h-3 mr-1"
-                                            aria-hidden="true" fill="currentColor" viewBox="0 0 320 512">
-                                            <path
-                                                d="M27.66 224h264.7c24.6 0 36.89-29.78 19.54-47.12l-132.3-136.8c-5.406-5.406-12.47-8.107-19.53-8.107c-7.055 0-14.09 2.701-19.45 8.107L8.119 176.9C-9.229 194.2 3.055 224 27.66 224zM292.3 288H27.66c-24.6 0-36.89 29.77-19.54 47.12l132.5 136.8C145.9 477.3 152.1 480 160 480c7.053 0 14.12-2.703 19.53-8.109l132.3-136.8C329.2 317.8 316.9 288 292.3 288z" />
-                                        </svg></a>
                                 </div>
                             </th>
                         </tr>
