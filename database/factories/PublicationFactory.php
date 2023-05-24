0882247a1c489044bf838a91bc339f6175b2ab5a
Herr-Hauptmann <tarik.horoz@gmail.com>
2023-05-24 23:50:56 +0200

    publication factory creates a separate file for each object

--- database/factories/PublicationFactory.php
+++ database/factories/PublicationFactory.php
@@ -4,18 +4,18 @@ namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Facades\Storage;
+use Illuminate\Http\File;
 
-/**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Publication>
- */
 class PublicationFactory extends Factory
 {
     public function definition()
     {
+        $documentName = 'example'.$this->faker->unique()->randomNumber($nbDigits = NULL, $strict = false).'.pdf';
+        $path = Storage::putFileAs('publications/', new File('resources/docs/example.pdf'), $documentName);
         return [
             'title' => $this->faker->unique()->bs,
             'description' => $this->faker->sentence($nbWords = 10, $variableNbWords = true),
-            'file_path' => 'publications/example.pdf',
+            'file_path' => 'publications/'.$documentName,
         ];
     }
 }
