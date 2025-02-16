c3cdbd68290712ddfa9d53fae067823b31f241c4
Herr-Hauptmann <tarik.horoz@gmail.com>
2025-02-16 20:51:18 +0100

    don't show background on mobile

---
+++ public/build/assets/Contact-C0RPuFJP.js
@@ -0,0 +1,1 @@
+import{_ as o}from"./GuestLayout-CPaW4I65.js";import t from"./Contact-DloruWs_.js";import{e as r,o as a}from"./app-B3F6_HyN.js";import"./portal-BpDlaEBJ.js";import"./keyboard-DhR-kLoq.js";import"./use-resolve-button-type-C1ILa-W3.js";import"./InputError-DdaNqAnR.js";const l=Object.assign({layout:o},{__name:"Contact",setup(m){return(_,c)=>(a(),r(t))}});export{l as default};
